f73060b30fe67b24fa3ae9b961851a47050ba52d
MESTREGUGABr <mestreguga24@gmail.com>
2019-09-17 14:57:12 -0300

    primeiro commit

---
+++ src/main/java/IFPE_3U_PROVA_EQUEIPE01/provinha/App.java
@@ -0,0 +1,13 @@
+package IFPE_3U_PROVA_EQUEIPE01.provinha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
